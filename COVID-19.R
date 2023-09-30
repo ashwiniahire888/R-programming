7bbf3901d1c5c40c75d84388bf9566952f802a6c
ashwiniahire888 <89189805+ashwiniahire888@users.noreply.github.com>
2023-09-30 20:33:57 +0530

    Add files via upload

---
+++ COVID-19.R
@@ -0,0 +1,183 @@
+Patient_id=c( "71-7021-5476-6001","71-7021-5476-6002","71-7021-5476-6003","71-7021-5476-6004","71-7021-5476-6005",
+              "71-7021-5476-6006","71-7021-5476-6007","71-7021-5476-6008","71-7021-5476-6009","71-7021-5476-6010","71-7021-5476-6011",
+              "71-7021-5476-6012","71-7021-5476-6013","71-7021-5476-6014","71-7021-5476-6015","71-7021-5476-6016","71-7021-5476-6017",
+              "71-7021-5476-6018","71-7021-5476-6019","71-7021-5476-6020","71-7021-5476-6021","71-7021-5476-6022","71-7021-5476-6023",
+              "71-7021-5476-6024","71-7021-5476-6025","71-7021-5476-6026","71-7021-5476-6027","71-7021-5476-6028","71-7021-5476-6029",
+              "71-7021-5476-6030","71-7021-5476-6031","71-7021-5476-6032","71-7021-5476-6033","71-7021-5476-6034","71-7021-5476-6035",
+              "71-7021-5476-6036","71-7021-5476-6037","71-7021-5476-6038","71-7021-5476-6039","71-7021-5476-6040","71-7021-5476-6041"
+              ,"71-7021-5476-6042","71-7021-5476-6043","71-7021-5476-6044","71-7021-5476-6045","71-7021-5476-6046","71-7021-5476-6047"
+              ,"71-7021-5476-6048","71-7021-5476-6049","71-7021-5476-6050","71-7021-5476-6051","71-7021-5476-6052","71-7021-5476-6053",
+              "71-7021-5476-6054","71-7021-5476-6055","71-7021-5476-6056","71-7021-5476-6057","71-7021-5476-6058","71-7021-5476-6059",
+              "71-7021-5476-6060")
+print(Patient_id)
+
+
+Reporting_date=c("07-06-2021","08-06-2021","09-06-2021","10-06-2021","07-06-2021","12-06-2021","13-06-2021","14-06-2021",
+                 "15-06-2021","11-06-2021","10-06-2021","18-06-2021","19-06-2021","20-06-2021","11-06-2021","22-06-2021","23-06-2021",
+                 "07-06-2021","25-06-2021","16-06-2021","27-06-2021","11-06-2021","29-06-2021","13-06-2021","01-07-2021",
+                 "02-07-2021","03-07-2021","01-07-2021","05-07-2021","06-07-2021","02-07-2021","08-07-2021","09-07-2021",
+                 "11-07-2021","11-07-2021","12-07-2021","13-07-2021","12-07-2021","15-07-2021","16-07-2021","17-07-2021",
+                 "18-07-2021","18-07-2021","20-07-2021","21-07-2021","18-07-2021","23-07-2021","24-07-2021","25-07-2021",
+                 "26-07-2021","25-07-2021","28-07-2021","29-07-2021","30-07-2021","30-07-2021","01-08-2021","02-08-2021",
+                 "03-08-2021","04-08-2021","05-08-2021")
+print(Reporting_date)
+
+
+Country=c("India","India","India","India","Brazil","Brazil","Brazil","Brazil","Brazil","France","France","France","France",
+          "France","France","United States","United States","United States","United States","United States","United States","United States","ndia",
+          "India","India","India","India","India","India","Germany","Germany","Germany","Germany","Germany","Germany","Germany","Germany",
+          "Germany", "Italy", "Italy", "Italy","Italy","Italy","Italy","Spain","Spain","Spain","Spain","Spain","Spain","Canada","Canada",
+          "Canada","Canada","Canada","Anada","Australia","Australia","Australia","Australia")
+print(Country)
+
+
+Location=c("Mumbai","Pune", "Nashik","Badoda","Bahia","Ceara","Goias","Para","Roraima","Mareseille","Lyon","Toulouse","Toulouse",
+           "Lyon","Lille","Washington","California","Washington","America","America", "London", "London", "Malegaon","Nagpur","Jaipur",
+           "Bhopal","Lakhnav","Aagra","Kathmandu","Baden","Bavaria","Humbrung","Hesse","Lower Saxony","Saarland","Saarland","Saarland","Humbrung",
+           "Rome","Milan","Naples","Florence","Venice","Turin","Madrid","Seville","Zaragoza","Zaragoza","Malaga","Murcia","Ottawa","Toronto",
+           "Halifax","Quebec","Quebec","Halifax","Canberra","Darwin","Darwin","Darwin")
+print(Location)
+
+
+Gender=c("M","F","F","F","F","F","M","M","F","M","F","F","M","M","M","M","M","M","M","F","F","F","F","F","F","M","M","M",
+         "F","F","M","M","M","F","F","F","M","F","F","M","F","M","F","M","F","M","F","M","F","M","F","F","F","F","M","M","M","M","F","F")
+print(Gender)
+
+
+Age=c(20,21,21,22,20,35,25,35,30,30,35,26,26,69,60,50,50,48,48,30,35,40,45,42,
+      24,42, 25,25, 25, 45 ,45,34,34,35,23,23,23,23,34,34,34,45,45,45,56,56,50,56,
+      22,22,22,22,70,70,72,72,72,75,75)
+print(Age)
+
+
+Recovered=c("TRUE","TRUE","FALSE","FALSE","FALSE","TRUE","TRUE","TRUE","TRUE","TRUE","TRUE","TRUE","TRUE","FALSE","FALSE","FALSE","FALSE",
+            "FALSE","FALSE","TRUE","TRUE","FALSE","FALSE","FALSE","TRUE","FALSE","TRUE","TRUE","TRUE","FALSE","FALSE","TRUE","TRUE","TRUE",
+            "TRUE","TRUE","TRUE","TRUE","TRUE","TRUE","TRUE","FALSE","FALSE","FALSE","FALSE","FALSE","FALSE","FALSE","TRUE","TRUE","TRUE",
+            "TRUE","FALSE","FALSE","FALSE","FALSE","FALSE","FALSE","FALSE","FALSE")
+print(Recovered)
+
+
+Death=c("FALSE","FALSE","TRUE","TRUE","TRUE","FALSE","FALSE","FALSE","FALSE","FALSE","FALSE","FALSE","FALSE","TRUE","TRUE",
+        "TRUE","TRUE","TRUE","TRUE","FALSE","FALSE","TRUE","TRUE","TRUE","FALSE","TRUE","FALSE","FALSE","FALSE","TRUE","TRUE","FALSE",
+        "FALSE","FALSE","FALSE","FALSE","FALSE","FALSE","FALSE","FALSE","FALSE","TRUE","TRUE","TRUE","TRUE","TRUE","TRUE","TRUE","FALSE",
+        "FALSE","FALSE","FALSE","TRUE","TRUE","TRUE","TRUE","TRUE","TRUE","TRUE","TRUE")
+print(Death)
+
+
+Clinical_Expenses=c(34000,55455,4343,30000,5656,54656,4534,20000,4000,3555,25000,40000,10000,15000,
+                    15000,15000,15000,15000,25000,20000,3000,4000,5000,10000,34000,40000,41000,20000,26000,15000,15000,
+                    7000,8000,9000,28000,2647,8000,50000,33000,23000,14000,22000,26000,34000,45000,29000,12000,14000,
+                    23000,34000,8000,7000,6000,23000,20000,20000,20000,20000,20000,30000)
+print(Clinical_Expenses)
+
+
+Symptons=c("Fever","Illness","Weakness","Cold","Cough","Tiredness","Tiredness","Tiredness","Tiredness","Tiredness","Tiredness","Tiredness",
+           "Cough","Cough","Cough","Cough","Cough","Coug","Cough","Cough","Loss of Taste","Loss of Taste","Loss of Taste","Loss of Taste","Loss of Taste","Loss of Taste",
+           "Loss of Taste","Loss of Taste","Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell",
+           "Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell",
+           "Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell","Loss of Smell","Headache","Headache",
+           "Headache","Headache","Headache","Headache","Headache","Fever","Fever")
+print(Symptons)
+
+M1=cbind(Patient_id,Reporting_date,Country,Location,Gender,Age,Recovered,Death,Clinical_Expenses,Symptons)
+print(M1)
+
+df1=data.frame(Patient_id,Reporting_date,Country,Location,Gender,Age,Recovered,Death,Clinical_Expenses,Symptons)
+print(df1)
+
+mytable1=data.frame(df1)
+mytable1=edit(mytable1)
+
+print(df1[1:2])
+print(df1[2:3])
+print(df1[2:4])
+print(df1[4,1:3])
+print(df1[4,1:5])
+print(df1[2,1:3])
+
+setwd("c://data")
+getwd()
+Ashwini=read.csv("c:/data/covid.csv")
+print(Ashwini)
+View(Ashwini)
+
+
+mean(Age)
+mean(Clinical_Expenses)
+
+median(Age)
+median(Clinical_Expenses)
+
+mode(Age)
+getmode <- function(Age) {
+  uniqv <- unique(Age)
+  uniqv[which.max(tabulate(match(Age, uniqv)))]
+}
+result <- getmode(Age)
+print(result)
+mode(Clinical_Expenses)
+getmode <- function(Clinical_Expenses) {
+  uniqv <- unique(Clinical_Expenses)
+  uniqv[which.max(tabulate(match(Clinical_Expenses, uniqv)))]
+}
+result <- getmode(Clinical_Expenses)
+print(result)
+
+
+
+
+
+range(Age)
+range(Clinical_Expenses)
+
+
+min(Age)
+min(Clinical_Expenses)
+
+max(Age)
+max(Clinical_Expenses)
+
+
+
+sort(Age)
+sort(Clinical_Expenses)
+
+
+var(Age)
+var(Clinical_Expenses)
+
+
+
+sd(Age)
+sd(Clinical_Expenses)
+
+
+sqrt(Age)
+sqrt(Clinical_Expenses)
+
+
+
+summary(Age)
+summary(Clinical_Expenses)
+
+plot(Age,type='l',main="Covid Patient",xlab="No of Patient",ylab="Patient Age",col=c("red"))
+
+boxplot(Age,main="covid patient",xlab="No of Patient",ylab="Patient 
+Age",col=c("Green","Red","Blue","Black","Pink","Yellow"))
+
+pie(Age,main="covid patient",xlab="No of Patient",ylab="Patient Age",col=c("Green"))
+
+barplot(Age,main="covid patient",xlab="No of Patient",ylab="Patient 
+(Age",col=c("Green","Red","Blue","Black","Pink","Yellow"))
+
+hist(Age,main="Covid Patient",xlab="No of patient",ylab="Patient 
+Age",col=c("Green","Red","Blue","Black","Pink","Yellow"))
+
+dotchart(Age,main="Covid Patient",xlab="No of Patient",ylab="Patient
+Age",col=c("Green","Red","Blue","Black","Pink","Yellow"))
+
+plot(Age,type='l',main="Covid Patient",xlab="No of Patient",ylab="Patient
+Age",col=c("Green"))
+
+polygon(Age,main="Covid Patient",xlab="No of Patient",ylab="Patient 
+Age",col=c("Green","Red","Blue","Black","Pink","Yellow"))
