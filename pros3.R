2f66a255dab0692d350a1334ec543acf8b56639e
ashwiniahire888 <89189805+ashwiniahire888@users.noreply.github.com>
2023-10-03 22:21:04 +0530

    Add files via upload

--- pros3.R
+++ pros3.R
@@ -1,6 +1,7 @@
-v=c(1,2,3,4,5)
+v=c(10,21,23,34,45)
 print(v)
 mean(v)
+median(v)
 var(v)
 sd(v)
 install.packages("readxl")
@@ -16,3 +17,54 @@ print(ashu)
 ashu<-read.table("c://data/voter.csv",header=T)
 summary(ashwini)
 range(ashwini$Age)
+Age=c(20,21,23,25,26)
+print(Age)
+Marks=c(58,56,65,78,45)
+print(Marks)
+mean(Age)
+mean(Marks)
+median(Age)
+median(Marks)
+mode(Age)
+getmode <- function(Age){
+  uniqv <- unique(Age)
+  uniqv[which.max(tabulate(match(Age,uniqv)))]
+}
+result <- getmode(Age)
+print(result)
+mode(Marks)
+getmode<-function(Marks){
+  uniqv<- unique(Marks)
+  uniqv[which.max(tabulate(match(Age,uniqv)))]
+}
+result <-getmode(Marks)
+print(result)
+range(Age)
+range(Marks)
+min(Age)
+min(Age)
+max(Age)
+max(Age)
+sort(Age)
+sort(Marks)
+var(Age)
+var(Marks)
+sd(Age)
+sd(marks)
+sqrt(Age)
+sqrt(Marks)
+summary(Age)
+summary(Marks)
+
+
+
+
+
+
+
+
+
+
+
+
+
